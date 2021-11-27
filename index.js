ea0480ab3417d196895cd7e528b7d03d5bb23047
didyou98 <didyou98@naver.com>
2021-11-27 14:28:14 +0900

    edit path3

--- index.js
+++ index.js
@@ -48,7 +48,9 @@ var app = express();
 app.use(bodyParser.urlencoded({extended : false}));
 
 
-app.listen(8080, () => {console.log('Server Running at http://localhost:8080')});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 
 //라우팅
 
